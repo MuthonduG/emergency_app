9fd8c0de894241490ad1e0c4e88fc5b36c006738
MuthonduG <muthondugithinji@gmail.com>
2025-01-10 01:40:34 -0500

    update: debugging dynamic user route

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -31,8 +31,7 @@ export const loginUser  = async (credentials: { email: string; userid: string })
 
     // Redirect based on role
     if (data.role === 'light') {
-      console.log(data)
-      window.location.href = `../User/${data.userid}`;
+      window.location.href = `../Users/${data.userid}`;
     } else if (data.role === 'shadow') {
       window.location.href = '../Dashboard/page';
     } else {
