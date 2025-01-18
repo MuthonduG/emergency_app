05af654fed7e62f27e07cbba5be1c70124d3746c
githinjiEssy <esthergithinji49@gmail.com>
2025-01-18 17:50:02 +0300

    side navbar update

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -9,65 +9,66 @@ import { MdCrisisAlert } from "react-icons/md";
 
 function Sidenav() {
   return (
-    <div className="group sticky top-0 left-0 bottom-0 h-screen bg-[#961f1f] text-white flex flex-col items-center py-4 w-[70px] hover:w-[240px] transition-all duration-300">
-      {/* Logo */}
-      <div className='p-4 w-full flex justify-center items-center'>
-        <MdCrisisAlert className='mx-auto h-10 w-auto rounded-lg p-3 bg-red-200 text-red-900'/>
-        <h3 className='text-xl font-bold content-center hidden group-hover:block'>QuickAid</h3>
+  <nav className="sticky flex flex-col items-center w-48 h-screen overflow-hidden text-gray-700 bg-gray-100 rounded">
+    <a className="flex items-center w-full px-3 mt-3" href="#">
+      <svg className="w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+        <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+      </svg>
+      <span className="ml-2 text-sm font-bold">Astroaid</span>
+    </a>
+
+    <div className="w-full px-2">
+      <div className="flex flex-col items-center w-full mt-3 border-t border-gray-300">
+        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
+          <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+          </svg>
+          <span className="ml-2 text-sm font-medium">Dasboard</span>
+        </a>
+        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300 text-black" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
+            <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/>
+          </svg>
+          <span className="ml-2 text-sm font-medium">Location</span>
+        </a>
+        <a className="flex items-center w-full h-12 px-3 mt-2 bg-gray-300 rounded" href="#">
+          <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+          </svg>
+          <span className="ml-2 text-sm font-medium">Departmens</span>
+        </a>
+        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
+            <path d="M720-400v-120H600v-80h120v-120h80v120h120v80H800v120h-80Zm-360-80q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm80-80h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0-80Zm0 400Z"/>
+          </svg>
+          <span className="ml-2 text-sm font-medium">Users</span>
+        </a>
       </div>
-      <div className="h-[90px] w-full p-4"></div>
-      {/* Menu Items */}
-      <ul className="flex flex-col items-center w-full space-y-2">
-        <li className="w-full">
-          <a href="/Dashboard" className="flex items-center gap-4 px-4 py-3 rounded-lg hover:bg-[#dba0a0] transition-all duration-300">
-            <FaGaugeHigh className="text-lg" />
-            <span className="text-lg font-medium whitespace-nowrap hidden group-hover:block">Dashboard</span>
-          </a>
-        </li>
-        <li className="w-full">
-          <a href="/Technician" className="flex items-center gap-4 px-4 py-3 rounded-lg hover:bg-[#dba0a0] transition-all duration-300">
-            <BsFillBuildingFill className="text-lg" />
-            <span className="text-lg font-medium whitespace-nowrap hidden group-hover:block">Technician</span>
-          </a>
-        </li>
-        <li className="w-full">
-          <a href="/Department" className="flex items-center gap-4 px-4 py-3 rounded-lg hover:bg-[#dba0a0] transition-all duration-300">
-            <BsFillBuildingFill className="text-lg" />
-            <span className="text-lg font-medium whitespace-nowrap hidden group-hover:block">Department</span>
-          </a>
-        </li>
-        <li className="w-full">
-          <a href="/Device" className="flex items-center gap-4 px-4 py-3 rounded-lg hover:bg-[#dba0a0] transition-all duration-300">
-            <FaMicrochip className="text-lg" />
-            <span className="text-lg font-medium whitespace-nowrap hidden group-hover:block">Device</span>
-          </a>
-        </li>
-        <li className="w-full">
-          <a href="/Location" className="flex items-center gap-4 px-4 py-3 rounded-lg hover:bg-[#dba0a0] transition-all duration-300">
-            <FaMicrochip className="text-lg" />
-            <span className="text-lg font-medium whitespace-nowrap hidden group-hover:block">Location</span>
-          </a>
-        </li>
-        <li className="w-full">
-          <a href="/Profile" className="flex items-center gap-4 px-4 py-3 rounded-lg hover:bg-[#dba0a0] transition-all duration-300">
-            <FaUserAlt className="text-lg" />
-            <span className="text-lg font-medium whitespace-nowrap hidden group-hover:block">Profile</span>
-          </a>
-        </li>
-        <li className="w-full">
-          <a href="/Setting" className="flex items-center gap-4 px-4 py-3 rounded-lg hover:bg-[#dba0a0] transition-all duration-300">
-            <IoMdSettings className="text-lg" />
-            <span className="text-lg font-medium whitespace-nowrap hidden group-hover:block">Settings</span>
-          </a>
-        </li>
-        <li className="absolute bottom-0 w-full mt-auto">
-          <a href="/Logout" className="flex items-center gap-4 px-4 py-3 rounded-lg hover:bg-[#dba0a0] transition-all duration-300">
-            <TbLogout className="text-lg" />
-            <span className="text-lg font-medium whitespace-nowrap hidden group-hover:block">LogOut</span>
-          </a>
-        </li>
-      </ul>
+      <div className="flex flex-col items-center w-full mt-2 border-t border-gray-300">
+        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
+            <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/>
+          </svg>
+          <span className="ml-2 text-sm font-medium">Profile</span>
+        </a>
+        <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-300" href="#">
+          <svg className="w-6 h-6 stroke-current"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+          </svg>
+          <span className="ml-2 text-sm font-medium">Settings</span>
+        </a>
+
+      </div>
     </div>
+    
+    <a className="flex items-center justify-center w-full h-16 mt-auto bg-gray-200 hover:bg-gray-300" href="#">
+      <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+      <span className="ml-2 text-sm font-medium">Account</span>
+    </a>
+  </nav>
+
   );
 }
 
